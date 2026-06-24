611fc96d6747482aeb518f46b17c2bf67e9c9bcb
davifriggiunifesp <davi.friggi@unifesp.br>
2026-06-23 22:16:38 -0300

    Create Comparator.ts for template base

---
+++ Comparator.ts
@@ -0,0 +1,28 @@
+abstract class Comparator {
+    public readonly nome: string;
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+
+    public exibirCabecalho(): void {
+        console.log(`\n=== ${this.nome} ===`);
+    }
+
+    public printar(resultado: string[]): void {
+        console.log(`> Resultado final: [ ${resultado.join(", ")} ]`);
+    }
+  
+    public ordenar(palavras: string[]): string[] {
+        this.exibirCabecalho();
+      
+        const copiaLista = [...palavras]; //novo array
+        copiaLista.sort((a, b) => this.compararCritério(a, b));
+
+        this.printar(copiaLista);
+        return copiaLista;
+    }
+
+    protected abstract compararCritério(a: string, b: string): number;
+}
+
