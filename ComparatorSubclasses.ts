b16bee040fc1a365e47b3f4cd27f67b0cd095b47
davifriggiunifesp <davi.friggi@unifesp.br>
2026-06-23 22:18:11 -0300

    Update ComparatorSubclasses.ts with super constructor

--- ComparatorSubclasses.ts
+++ ComparatorSubclasses.ts
@@ -1,16 +1,30 @@
+import Comparator from './Comparator'
+
 export const compararAlfabetico extends Comparator = (a: string, b: string): number => {
+    constructor(name: string) {
+        super(name);
+    }
+    
     protected compararCritério(a: string, b: string): number {
         return a.localeCompare(b);
     }
 };
 
 export const compararTamanho extends Comparator = (a: string, b: string): number => {
+    constructor(name: string) {
+        super(name);
+    }
+    
     protected compararCritério(a: string, b: string): number {
         return a.length - b.length;
     }
 };
 
 export const compararUltimaLetra extends Comparator = (a: string, b: string): number => {
+    constructor(name: string) {
+        super(name);
+    }
+    
     protected compararCritério(a: string, b: string): number {
         if (a.length === 0) return -1;
         if (b.length === 0) return 1;
